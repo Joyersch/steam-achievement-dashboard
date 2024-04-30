b5031418a9776fbe96ca4f3115cd8fecc332616b
Joy Schnittker <dev@joyersch.de>
2024-04-30 22:25:21 +0200

    Added models and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,30 +18,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
-        'password',
+        'steam_user_id'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function gameStats()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(UserGameStat::class);
     }
 }
