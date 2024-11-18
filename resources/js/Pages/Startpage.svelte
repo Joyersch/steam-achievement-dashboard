03cc7b2ffec4d5e5a2fd88db06b964f7b0dce7cf
Joy Schnittker <dev@joyersch.de>
2024-11-18 12:28:13 +0100

    Fixed startpage mobile view

--- resources/js/Pages/Startpage.svelte
+++ resources/js/Pages/Startpage.svelte
@@ -7,7 +7,7 @@
     <div class="flex justify-center content-center flex-col">
         <div class="container mx-auto bg-paper" style="max-width: 750px;">
             <div class="flex justify-center mt-3 mb-3 font-grandstander">
-                <h1 class="text-4xl">Acitivies of the last month</h1>
+                <h1 class="text-4xl ml-3 mr-3">Acitivies of the last month</h1>
             </div>
             <div class="font-rubik">
                 <Activities {data} />
